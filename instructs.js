adfdad3b2aaeddb3cd2a49c8aba38283fa27f420
viyiviyi <39589598+viyiviyi@users.noreply.github.com>
2019-02-28 16:19:51 +0800

    Add files via upload

--- instructs.js
+++ instructs.js
@@ -2,52 +2,59 @@
     仓库，用来放需要传递给别的组件使用的方法
 */
 export class Storage {
-    constructor() {
+    constructor(info, arr, isLog) {
         this.state = {}
         this.instructs = []
+        this.isLog = isLog
     }
     add = (fun, instructName, component) => {
         this.state[instructName] = {
             fun,
-            component: component||true
+            component: component || true
         }
         this.instructs.push(instructName)
     }
     handelRun = (instructName, ...parameter) => {
-        if (!this.state[instructName]) return () => {
-            console.log(`${instructName} is undefined or Not loaded`)
-        }
-        if (!this.state[instructName].component) return () => {
-            console.log(`The target component is not loaded or has been unloaded`)
-        }
+        if (!this.state[instructName]) this.isLog && console.log(`${instructName} is undefined or Not loaded`)
+        
+        if (!this.state[instructName].component) this.isLog && console.log(`The target component is not loaded or has been unloaded`)
+        
         return this.state[instructName].fun.apply(this.state[instructName].component, parameter)
     }
-    handelSyncRun = (instructName,...parameter) => {
-        if (!this.state[instructName]) return [0, `${instructName} is undefined or Not loaded`]
-        if (!this.state[instructName].component) return [0, `The target component is not loaded or has been unloaded`]
-        return [1, this.state[instructName].fun.apply(this.state[instructName].component, parameter)]
+    handelAsyncRun = (instructName, ...parameter) => {
+        if (!this.state[instructName]) return [false, `${instructName} is undefined or Not loaded`]
+        if (!this.state[instructName].component) return [false, `The target component is not loaded or has been unloaded`]
+        return [true, this.state[instructName].fun.apply(this.state[instructName].component, parameter)]
     }
-    run = (instructName) => {
-        return (par) => {
-            if(this.handelRun(instructName, par)) return this.handelRun(instructName, par)()
+    run = (instructName,...tar) => {
+        return (...par) => {
+            if (this) return this.handelRun(instructName, ...tar, ...par)
         }
     }
-    syncRun = (instructName) => {
-        return (par) => {
-            return new Promise((res, rej) => {
-                let st = this.handelSyncRun(instructName, par)
-                st[0] ? res(st[1]) : rej(st[1])
-            })
+
+    asyncRes = (instructName, ...par) => {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                let st = this.handelAsyncRun(instructName, ...par)
+                st[0] && res(st[1])
+                this.isLog && console.log(st[1])
+            }, 0);
+        })
+    }
+
+    asyncRun = (instructName,...tar) => {
+        return (...par) => {
+            return this.asyncRes(instructName,...tar, ...par)
         }
     }
 }
 
-let init = (info) => {
+let init = (info, arr, isLog) => {
     let _s = {}
-    let storage = new Storage(info)
+    let storage = new Storage(info, arr, isLog)
     _s.add = storage.add
     _s.run = storage.run
-    _s.syncRun = storage.syncRun
+    _s.asyncRun = storage.asyncRun
     _s.list = storage.instructs
     Array.isArray(info) && info.forEach(item => {
         _s[Object.keys(item)[0]] = item.Object.keys(item)[0]
